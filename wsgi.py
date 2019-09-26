df54a85fecbd6ece357c49570b7e4cc27dbfc4e0
gilles vanbrutzel <tnhl2209@gmail.com>
2019-09-26 15:54:41 +0200

    reorder import

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-from models import Product
 from flask_marshmallow import Marshmallow # Order is important here!
+db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+from models import Product
+from schemas import products_schema
 
 @app.route('/hello')
 def hello():
@@ -21,5 +23,9 @@ from models import Product
 from schemas import products_schema
 @app.route('/products')
 def products():
+
+    # data = requect.get_json()
+    # name = data.get("name")
+    #description = data.get("description)")
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
