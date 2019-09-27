e12dda47b00a5b80b3cd841887114debadb84159
gilles vanbrutzel <tnhl2209@gmail.com>
2019-09-27 11:26:21 +0200

    renderer template

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 import os
 import logging
 #logging.warn(os.environ["DUMMY"])
-from flask import Flask
+from flask import Flask, abort, request
+from flask import Flask, render_template
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,20 +13,43 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
+    products = db.session.query(Product).all()
 
+    return render_template('home.html', products=products)
 
-from models import Product
-from schemas import products_schema
 @app.route('/products')
-def products():
-
-    # data = requect.get_json()
-    # name = data.get("name")
-    #description = data.get("description)")
+def read_products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/products/<int:id>')
+def read_product(id):
+    product = db.session.query(Product).get(id) # SQLAlchemy request => 'SELECT * FROM products'
+    if product is None:
+        abort(404)
+    else :
+        return product_schema.jsonify(product)
+
+@app.route('/<int:id>')
+def product_html(id):
+    product = db.session.query(Product).get(id)
+    return render_template('product.html', product=product)
+
+@app.route('/products', methods = ['POST'])
+def create_products():
+
+    name = request.json['name']
+    description = request.json['description']
+
+    product  = product(name = name, description = description)
+
+    db.session.add(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product), 201
+
+
